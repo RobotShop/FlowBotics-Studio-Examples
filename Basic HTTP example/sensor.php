4e21adb38f1faebcbc19ca70e4426e1608af0eab
Sébastien Parent-Charette <scharette@robotshop.com>
2018-07-18 09:31:28 -0400

    Added a few basic exmaples and HTTP example

---
+++ Basic HTTP example/sensor.php
@@ -0,0 +1,16 @@
+<?php
+echo '' . "\r\n";
+echo '*** lightSensor' . "\r\n";
+echo 'logicalName: ""' . "\r\n";
+echo 'advertisedValue: "17.5"' . "\r\n";
+echo 'unit: "lx"' . "\r\n";
+echo 'currentValue: 17.6' . "\r\n";
+echo 'lowestValue: 3.5' . "\r\n";
+echo 'highestValue: 82.6' . "\r\n";
+echo 'currentRawValue: 17.617' . "\r\n";
+echo 'logFrequency: 1/s' . "\r\n";
+echo 'reportFrequency: OFF' . "\r\n";
+echo 'calibrationParam: 0,' . "\r\n";
+echo 'resolution: 0.1' . "\r\n";
+echo 'measureType: HUMAN_EYE' . "\r\n";
+?>
